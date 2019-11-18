8fb975c00387cfdf6ba6d1c6836dc46897c5ae56
wildyeo58 <lsolor94@gmail.com>
2019-11-17 18:58:38 -0600

    more recurse practice

--- coin_rec.py
+++ coin_rec.py
@@ -1,6 +1,11 @@
 def coins(n):
     cache = [0] * (n + 1)
     cache[0] = 1
+    cache[1] = 1
+    cache[2] = 1
+    cache[3] = 1
+    cache[4] = 1
+
     cache_helper(n, cache)
     print(cache)
     return
@@ -10,17 +15,19 @@ def cache_helper(n, cache):
     if cache[n] != 0:
         return cache[n]
     print("on %d" %n)
-    if n-1 >= 0:
-        print('add 1')
-        cache[n] += cache_helper(n-1, cache)
+
+    nickel = 0
+    dime = 0
+    quarter = 0
     if n-5 >= 0:
-        cache[n] += cache_helper(n-5, cache)
+        nickel = cache_helper(n-5, cache)
         print('add 5')
     if n-10 >= 0:
-        cache[n] += cache_helper(n-10, cache)
+        dime = cache_helper(n-10, cache)
     if n-25 >= 0:
-        cache[n] += cache_helper(n-25, cache)
+        quarter = cache_helper(n-25, cache)
+    cache[n] = 1 + nickel + dime + quarter
     return cache[n]
 
 if __name__ == '__main__':
-    print(coins(6))
+    print(coins(26))
